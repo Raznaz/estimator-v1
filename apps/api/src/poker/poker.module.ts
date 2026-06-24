a6b60b665e0745db0845f5a006914330d4d70cde
nazraz <nazzzarr@gmail.com>
2026-06-24 21:47:19 +0300

    init

--- apps/api/src/poker/poker.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PokerGateway } from './poker.gateway';
-import { PokerService } from './poker.service';
-
-@Module({
-  providers: [PokerGateway, PokerService],
-})
-export class PokerModule {}
--- apps/web/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  // Транспилируем общий пакет (исходники TS вместо предсобранного билда)
-  transpilePackages: ['@estimator/shared'],
-};
-
-export default nextConfig;
